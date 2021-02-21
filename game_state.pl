c6874e9949061147135f7c8743b978055063fedb
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2021-02-21 21:59:55 +0100

    Basic notebook allowing to interact with prolog program

---
+++ game_state.pl
@@ -0,0 +1,20 @@
+position(dummy1, 0, 0).
+position(dummy2, 0, 1).
+
+stat(dummy1, ws, 35).
+stat(dummy2, ws, 35).
+
+stat(dummy1, s, 40).
+stat(dummy2, s, 40).
+
+skill(dummy1, meleeBasic, 5).
+skill(dummy2, twohanded, 5).
+
+advantage(dummy1, 0).
+advantage(dummy2, 0).
+
+race(dummy1, human).
+race(dummy2, dwarf).
+
+weapon(dummy1, handWeapon).
+weapon(dummy2, bastard).
